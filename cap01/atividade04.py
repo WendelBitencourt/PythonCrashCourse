7e566a5ac291f2436474a49c26b002c8e5b6aa17
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2024-07-22 00:38:44 -0300

    Atividade 04 - Capítulo 01

---
+++ cap01/atividade04.py
@@ -0,0 +1,7 @@
+
+autor = "Vagabond"
+frase = "Preocupado com uma única folha, você não verá a árvore. Preocupado com uma única árvore, você não verá a floresta."
+mensagem = f"No manga {autor} existe uma frase bem impactante que diz: {frase}"
+
+
+print(mensagem)
