e190318850fb51ade8485c93786c37409a24eb4f
Wendel Adriano Bitencourt <wendeladriano@protonmail.com>
2024-07-22 00:32:53 -0300

    Atividade 03 - Capitulo 01

---
+++ atividade03.py
@@ -0,0 +1,6 @@
+#Famous Quote
+
+autor = "Vagabond"
+frase = "Preocupado com uma única folha, você não verá a árvore. Preocupado com uma única árvore, você não verá a floresta."
+
+print(f"No manga {autor} existe uma frase bem impactante que diz: {frase}")
